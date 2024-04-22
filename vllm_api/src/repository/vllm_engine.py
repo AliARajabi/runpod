c96dfdeecbc64daaccf6fa8f901386bd17e82db6
a.rajabi <arajabi@basalam.in>
2024-04-22 10:08:56 +0330

    last version

--- vllm_api/src/repository/vllm_engine.py
+++ vllm_api/src/repository/vllm_engine.py
@@ -6,8 +6,8 @@ from vllm import LLM
 
 # enforce_eager=True
 # llm = LLM(model=config.hf_model, gpu_memory_utilization=0.9, trust_remote_code=True)
-llm = LLM(model='BaSalam/Llama2-7b-entity-attr-v5',
-          gpu_memory_utilization=0.95, trust_remote_code=True)
+llm = LLM(model='BaSalam/Llama2-7b-entity-attr-v5',tensor_parallel_size=1,
+          gpu_memory_utilization=0.97, trust_remote_code=True, download_dir='/workspace/runpod/models')
 
 tokenizer = AutoTokenizer.from_pretrained(
     'BaSalam/Llama2-7b-entity-attr-v5')
--- vllm_api/vllm_api/src/repository/vllm.py
+++
@@ -1,4 +0,0 @@
-from vllm import LLM, SamplingParams
-from configs.configs import config
-
-llm = LLM(model=config.hf_model, gpu_memory_utilization=0.9, trust_remote_code=True)
