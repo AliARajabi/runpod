0e1c28ba2d4457a8088d21dba5a05ea3c83545a8
AliARajabi <41513077+AliARajabi@users.noreply.github.com>
2024-04-30 11:43:42 +0330

    Update vllm_engine.py

--- vllm_api/src/repository/vllm_engine.py
+++ vllm_api/src/repository/vllm_engine.py
@@ -2,7 +2,7 @@ from huggingface_hub import login
 from transformers import AutoTokenizer
 from vllm import LLM
 
-# login(token='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')
+login(token='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')
 
 # enforce_eager=True
 # llm = LLM(model=config.hf_model, gpu_memory_utilization=0.9, trust_remote_code=True)
