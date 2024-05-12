ac4264d34e7b403c4d2d4d0b75e208d719a3a5d2
AliARajabi <41513077+AliARajabi@users.noreply.github.com>
2024-05-12 11:41:53 +0330

    Update vllm_engine.py

--- vllm_api/src/repository/vllm_engine.py
+++ vllm_api/src/repository/vllm_engine.py
@@ -2,11 +2,11 @@ from huggingface_hub import login
 from transformers import AutoTokenizer
 from vllm import LLM
 
-login(token='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')
+login(token='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')
 
 # enforce_eager=True
 # llm = LLM(model=config.hf_model, gpu_memory_utilization=0.9, trust_remote_code=True)
-llm = LLM(model='BaSalam/Llama2-7b-entity-attr-v5',tensor_parallel_size=1,
+llm = LLM(model='alirajabi/Llama2-7b-entity-attr-v5',tensor_parallel_size=1,
           gpu_memory_utilization=0.97, trust_remote_code=True, download_dir='/workspace/runpod/models')
 
 tokenizer = AutoTokenizer.from_pretrained(
